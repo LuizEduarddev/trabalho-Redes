d3be1e93f90e3949c5db01b46e49b4c82b3f7684
Luiz <luiz.dias@perpetuo.g12.br>
2023-11-21 20:03:24 -0300

    Necessário alterar todas as linhas que contem a funcao 'servidor.send()' e adicionar o '.encode(UTF-8)' no final, se nao vai bugar

--- Servidor/servidor.py
+++ Servidor/servidor.py
@@ -12,86 +12,108 @@ def defineServidor():
     servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     return servidor
 
-def desfragmentaString(clientes, data: str, address):
+def desfragmentaString(clientes, data: str, address, servidor: socket.socket):
     try:
         data = data.split(' ')
     except:
-        print('ERR INVALID_MESSAGE_FORMAT')
+        servidor.send('ERR INVALID_MESSAGE_FORMAT')
 
     if data[0] == 'REG':
-        socketREG(clientes, data, address)
+        socketREG(clientes, data, address, servidor)
 
     elif data[0] == 'UPD':
-        socketUPD(clientes, data, address)
+        socketUPD(clientes, data, address, servidor)
 
     elif data[0] == 'LST':
-        socketLST(clientes, data, address)
+        socketLST(clientes, data, address, servidor)
 
     elif data[0] == 'END':
-        socketEND(clientes, data, address)
+        socketEND(clientes, data, address, servidor)
 
     else:
-        print('ERR INVALID_MESSAGE_FORMAT')
+        servidor.send('ERR INVALID_MESSAGE_FORMAT')
         return 0
 
-def requirementsREG(string: str):
+def verificaTemp(string, number, tamanho):
+    temp = number + 1
+    if temp >= tamanho:
+        return 
+    else:
+        string = string + ','
+
+def socketLST(clientes: dict, servidor: socket.socket):
+    number = int(0)
+    string = str()
+    tamanho = len(clientes)
+
+    for chave, info in clientes.items():
+        number += 1
+        string = string + f'{info["data"]},ip{info["users_count"]}:{info["address"]}'
+        verificaTemp(string, number, tamanho)
+    
+    servidor.send(string)
+
+def requirementsREG(string: str, servidor: socket.socket):
     string = string.split(' ')
     if len(string) < 4:
-        print('ERR INVALID_MESSAGE_FORMAT')
+        servidor.send('ERR INVALID_MESSAGE_FORMAT')
         exit()
     else:
         port = int(string[2])
         if port > 65535:
-            print('ERR PORT_INVALID_NUM')
+            servidor.send('ERR PORT_INVALID_NUM')
             exit()
         else:
             data = string[4]
             data.split(';')
             tamanho = len(data)
-            print(f'OK <{tamanho}>_REGISTERED_FILES')
+            servidor.send(f'OK <{tamanho}>_REGISTERED_FILES')
             return string
 
-def requirementsUPD(string: str):
+def requirementsUPD(string: str, servidor: socket.socket):
     string = string.split(' ')
     if len(string) < 4:
-        print('ERR INVALID_MESSAGE_FORMAT')
+        servidor.send('ERR INVALID_MESSAGE_FORMAT')
         exit()
     else:
         port = int(string[2])
         if port > 65535:
-            print('ERR PORT_INVALID_NUM')
+            servidor.send('ERR PORT_INVALID_NUM')
             exit()
         else:
-            data = string[4]
+            data = string[3]
             data.split(',')
             return string
 
-def searchInDB(clientes:dict, data:str, password: str):
-    for info in clientes.items():
+def searchInDB(clientes:dict, data:str, password: str, servidor: socket.socket):
+    for temp, info in clientes.items():
         if "password" in info and info["password"] == password:
             info["data"] = data
             data = data.split(';')
-            print(f'OK <{len(data)}>_REGISTERED_FILES')
+            servidor.send(f'OK <{len(data)}>_REGISTERED_FILES')
             return 
         
-    print('ERR IP_REGISTERED_WITH_DIFFERENT_PASSWORD')
+    servidor.send('ERR IP_REGISTERED_WITH_DIFFERENT_PASSWORD')
     return 
         
-def socketUPD(clientes:dict, data:str, address):
+def socketUPD(clientes:dict, data:str, address, servidor: socket.socket):
     data = requirementsUPD(data)
     password = data[1]
-    searchInDB(clientes, data, password)
+    searchInDB(clientes, data, password, servidor)
     
-def socketREG(clientes:dict, data: str, address):
-    data = requirementsREG(data)
-    updateClientes(clientes, data[3], address, data[2], data[1])
+def socketREG(clientes:dict, data: str, address, servidor: socket.socket):
+    data = requirementsREG(data, servidor)
+    clientes = updateClientes(clientes, data[3], address, data[2], data[1], servidor)
 
-def updateClientes(clientes :dict, data, address, port, password):
+def updateClientes(clientes :dict, data, address, port, password, servidor: socket.socket):
     atualizaNumero = 0
     atualizaNumero += int(clientes['usuarios'])
     clientes.update({'users_count': atualizaNumero})
     chave = f'client{atualizaNumero}'
     clientes.update({chave:{'data':data, 'address': address, 'port': port, 'password': password}})
+    register = data[3]
+    register = register.split(';')
+    servidor.send(f'OK <{len(register)}>_REGISTERED_FILES')
     return clientes
 
 def main():
@@ -103,13 +125,13 @@ def main():
     servidor.bind((HOST, PORTA))
     servidor.listen()
     print('Aguardando conexao....')
-    conn, ender = servidor.accept() # <-- AQUI
+    conn, address = servidor.accept() 
     
     while True:
-        data, address = servidor.recv(1024) # <-- ALTERAR TODO O CÓDIGO, NÓS JA RECEBEMOS O ENDEREÇO NA LINHA 106
+        data  = servidor.recv(1024) 
 
         print(f'Connection ready with IP: {address}')
-        desfragmentaString(clientes, data, address)
+        desfragmentaString(clientes, data, address, servidor)
 
         
     
